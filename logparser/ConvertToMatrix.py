d2eb915786bcb57c08ff711228536d6d20ba20d5
paquc <carl.paquet@leadsonline.com>
2024-10-25 09:56:48 -0400

    Add scripts to count alarms types

--- logparser/ConvertToMatrix.py
+++ logparser/ConvertToMatrix.py
@@ -8,8 +8,9 @@ df = pd.read_csv(input_file, header=None, names=["EventID", "Description", "Coun
 output_df = df.set_index("EventID").T.drop("Description")
 
 # Save the output to a new CSV file
-output_file = './BGL_Brain_results/BGL_Events_Matrix.csv'  # Replace with your desired output file name
-output_df.to_csv(output_file, index=False)
+output_file = './BGL_Brain_results/BGL_Events_Matrix_Tabs.csv'  # Replace with your desired output file name
+#output_df.to_csv(output_file, index=False)
+output_df.to_csv(output_file, sep='\t', index=False)
 
 # Display the result
 print(output_df)
