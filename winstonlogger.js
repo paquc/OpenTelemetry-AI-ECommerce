28cf62b7cbc83bf18540428d49f4ccc251da3bff
paquc <carl.paquet@leadsonline.com>
2024-11-25 15:12:58 -0500

    Clean-up windows logger

--- winstonlogger.js
+++ winstonlogger.js
@@ -10,14 +10,6 @@ const csvFormat = format.printf(({ level, message, timestamp }) => {
   return `${timestamp},${level},${message}`;
 });
 
-const fileRotateTransport = new winston.transports.DailyRotateFile({
-  filename: 'combined-%DATE%.log',
-  datePattern: 'YYYY-MM-DD',
-  zippedArchive: true,
-  maxSize: '20m',
-  maxFiles: '1d',
-});
-
 function createLogger(logFilePath) {
   return winston.createLogger({
     level: 'debug',
@@ -36,20 +28,13 @@ function createLogger(logFilePath) {
         maxSize: '2m',
         maxFiles: '1d',
       })
-      // new winston.transports.File({
-      //   filename: logFilePath,
-      // }),
-      // fileRotateTransport,
     ],
   });
 }
 
-// const msg = `${currentTimeUnix},${ERROR_NONE},${SOURCE_SERVICE},${API_ENDPOINT},${duration},,Users list fetched successfully from user-service in ${duration} ms`;
-
 function createMessage(currentTimeUnix, errorType, sourceService, apiEndpoint, val1, val2, message, request_ID = '') {
   return `${currentTimeUnix},${errorType},${sourceService},${apiEndpoint},${val1},${val2},${message},${request_ID}`;
 }
-
 
 module.exports = 
 {
