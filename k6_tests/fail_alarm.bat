2d7eb254528eeac8e5c2d0c68ff230db1c980ec3
paquc <carl.paquet@leadsonline.com>
2024-11-28 14:22:30 -0500

    Update K6 tests

--- k6_tests/fail_alarm.bat
+++ k6_tests/fail_alarm.bat
@@ -1,7 +1,7 @@
 :: Used to generate a constant load on the system and some errors
 
 set TEST_TYPE=1
-set TEST_SLEEP_DURATION=1
+set TEST_SLEEP_DURATION=5
 set TEST_DURATION=5s
 set NUMBER_OF_USERS=1
 k6 run .\use_system.js 
@@ -9,12 +9,12 @@ k6 run .\use_system.js
 :: BREAKPOINT test
 set TEST_SLEEP_DURATION=0
 set TEST_TYPE=5
-set RAMPS_UP_PERIOD=1s
-set RAMP_TARGET=50
+set RAMPS_UP_PERIOD=3s
+set RAMP_TARGET=10
 k6 run .\use_system.js 
 
 set TEST_TYPE=1
-set TEST_SLEEP_DURATION=1
+set TEST_SLEEP_DURATION=3
 set TEST_DURATION=15s
 set NUMBER_OF_USERS=1
 k6 run .\use_system.js 
