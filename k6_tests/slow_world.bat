21a6c5697f8d8d8b6442ce184f909b98dc6189cf
paquc <carl.paquet@leadsonline.com>
2024-11-29 23:44:13 -0500

    Add slow test

---
+++ k6_tests/slow_world.bat
@@ -0,0 +1,41 @@
+:: SMOKE TEST
+set TEST_TYPE=1
+set TEST_SLEEP_DURATION=1
+set TEST_DURATION=3s
+set NUMBER_OF_USERS=1
+k6 run .\use_system.js 
+
+set TEST_TYPE=1
+set TEST_SLEEP_DURATION=1
+set TEST_DURATION=5s
+set NUMBER_OF_USERS=1
+k6 run .\use_system.js 
+
+:: SPIKE Test
+set TEST_TYPE=3
+set TEST_SLEEP_DURATION=1
+set RAMPS_UP_PERIOD=10s
+set RAMP_TARGET=5
+k6 run .\use_system.js 
+
+:: BREAKPOINT test
+set TEST_TYPE=4
+set TEST_SLEEP_DURATION=0
+set RAMPS_UP_PERIOD=5s
+set RAMP_TARGET=200
+k6 run .\use_system.js 
+
+:: Used to generate a constant load on the system and some errors
+:: BREAKPOINT test
+set TEST_TYPE=5
+set TEST_SLEEP_DURATION=1
+set RAMPS_UP_PERIOD=5s
+set RAMP_TARGET=500
+k6 run .\use_system.js 
+
+set TEST_TYPE=1
+set TEST_SLEEP_DURATION=1
+set TEST_DURATION=5s
+set NUMBER_OF_USERS=1
+k6 run .\use_system.js 
+
