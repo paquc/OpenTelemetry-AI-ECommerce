b95a288c4f6eed0af8eef1ac52631ecc62ae2515
paquc <carl.paquet@leadsonline.com>
2024-12-04 08:19:00 -0500

    Adapt k6 real world test for long runs

--- k6_tests/test_real_world.bat
+++ k6_tests/test_real_world.bat
@@ -1,28 +1,28 @@
 :: SMOKE TEST
 set TEST_TYPE=1
-set TEST_SLEEP_DURATION=2
-set TEST_DURATION=1800s
+set TEST_SLEEP_DURATION=3
+set TEST_DURATION=6000s
 set NUMBER_OF_USERS=1
 k6 run .\use_system.js 
 
 set TEST_TYPE=1
-set TEST_SLEEP_DURATION=2
-set TEST_DURATION=1800s
-set NUMBER_OF_USERS=3
+set TEST_SLEEP_DURATION=3
+set TEST_DURATION=6000s
+set NUMBER_OF_USERS=5
 k6 run .\use_system.js 
 
 :: SPIKE Test
 set TEST_TYPE=3
 set TEST_SLEEP_DURATION=2
-set RAMPS_UP_PERIOD=1800s
+set RAMPS_UP_PERIOD=3600s
 set RAMP_TARGET=5
 k6 run .\use_system.js 
 
 :: STRESS test  
 set TEST_TYPE=4
-set TEST_SLEEP_DURATION=0
-set RAMPS_UP_PERIOD=1000s
-set RAMP_TARGET=200
+set TEST_SLEEP_DURATION=1
+set RAMPS_UP_PERIOD=5000s
+set RAMP_TARGET=50
 k6 run .\use_system.js 
 
 set TEST_TYPE=1
@@ -35,47 +35,47 @@ k6 run .\use_system.js
 :: BREAKPOINT test
 set TEST_TYPE=5
 set TEST_SLEEP_DURATION=0
-set RAMPS_UP_PERIOD=30s
-set RAMP_TARGET=500
+set RAMPS_UP_PERIOD=500s
+set RAMP_TARGET=200
 k6 run .\use_system.js 
 
 set TEST_TYPE=1
 set TEST_SLEEP_DURATION=2
-set TEST_DURATION=500s
-set NUMBER_OF_USERS=3
+set TEST_DURATION=4000s
+set NUMBER_OF_USERS=5
 k6 run .\use_system.js 
 
 :: STRESS test  
 set TEST_TYPE=4
 set TEST_SLEEP_DURATION=2
-set RAMPS_UP_PERIOD=60s
+set RAMPS_UP_PERIOD=6000s
 set RAMP_TARGET=100
 k6 run .\use_system.js 
 
 :: Used to generate a constant load on the system and some errors
 :: BREAKPOINT test
 set TEST_TYPE=5
-set TEST_SLEEP_DURATION=1
-set RAMPS_UP_PERIOD=20s
+set TEST_SLEEP_DURATION=0
+set RAMPS_UP_PERIOD=200s
 set RAMP_TARGET=100
 k6 run .\use_system.js 
 
 set TEST_TYPE=1
-set TEST_SLEEP_DURATION=1
-set TEST_DURATION=600s
+set TEST_SLEEP_DURATION=2
+set TEST_DURATION=1000s
 set NUMBER_OF_USERS=1
 k6 run .\use_system.js 
 
 :: STRESS test  
 set TEST_TYPE=4
 set TEST_SLEEP_DURATION=1
-set RAMPS_UP_PERIOD=120s
+set RAMPS_UP_PERIOD=1200s
 set RAMP_TARGET=50
 k6 run .\use_system.js 
 
 set TEST_TYPE=1
-set TEST_SLEEP_DURATION=2
-set TEST_DURATION=100s
+set TEST_SLEEP_DURATION=4
+set TEST_DURATION=3000s
 set NUMBER_OF_USERS=1
 k6 run .\use_system.js 
 
